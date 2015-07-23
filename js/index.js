b9a93e35769e4f22d3828cf5efb626f63d81f3f9
David Hou <david.hou314@gmail.com>
2015-07-22 23:25:55 -0700

    Add rudimentary new index page

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var images = [];
+for (var i = 0; i <= 14; i++) {
+    images[i] = new Image();
+    images[i].src = 'images/slideshow/' + i + '.jpg';
+}
+var currimage = 0;
+
+function nextImage() {
+    currimage++;
+    currimage %= 15;
+    $('#index_heading_back_background').css('background-image', 'url(' + images[currimage].src + ')');
+    $('#index_heading_background').fadeOut(1000, function () {
+        $(this).css('background-image', 'url(' + images[currimage].src + ')');
+        $(this).toggle(0, function () {
+            setTimeout(nextImage, 5000);
+        });
+    });
+}
+
+$(document).ready(function () {
+    setTimeout(nextImage, 5000);
+});
