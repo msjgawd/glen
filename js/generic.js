5fe68840c1a51ad14bc1f10cfb4a7e0416a04b34
David Hou <david.hou314@gmail.com>
2015-07-26 14:34:48 -0700

    Make msjwave.com link actually work

--- js/generic.js
+++ js/generic.js
@@ -9,7 +9,7 @@ function adjust_sizes() {
 }
 
 $(document).ready(function () {
-    var footerhtml = '<div class="footer" id="footer" style="background-color: #d3d3d3"><div class="copyright_by center_vertical height100p"><p class="footer">Created by <a class="footer" href="https://www.msjwave.com">MSJWAVE 2015</a></p></div></div>';
+    var footerhtml = '<div class="footer" id="footer" style="background-color: #d3d3d3"><div class="copyright_by center_vertical height100p"><p class="footer">Created by <a class="footer" target="_blank" href="http://msjwave.com">MSJWAVE 2015</a></p></div></div>';
     $('body').append(footerhtml);
 
     adjust_sizes();
