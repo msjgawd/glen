78534af2d03f75d80525a1ab10864d65357c3adc
David Hou <david.hou314@gmail.com>
2015-07-31 15:15:59 -0700

    Add navigation dots to homepage slideshow

--- js/index.js
+++ js/index.js
@@ -3,20 +3,48 @@ for (var i = 0; i <= 13; i++) {
     images[i] = new Image();
     images[i].src = 'images/slideshow/' + i + '.jpg';
 }
-var currimage = 0;
+
+var nextimage = 1;
+var timers = [];
+var lock = 0;
 
 function nextImage() {
-    currimage++;
-    currimage %= 14;
-    $('#index_heading_back_background').css('background-image', 'url(' + images[currimage].src + ')');
+    lock++;
+    var mylock = lock;
+    $('#index_heading_background').stop(true, true);
+    nextimage %= images.length;
+    for (var i = timers.length - 1; i >= 0; i--) {
+        clearTimeout(timers[i]);
+        timers.pop();
+    }
+    $('#index_slideshow_nav_image_' + nextimage).addClass('selected').parent().siblings().children().removeClass('selected');
+    if (mylock != lock) return;
+    $('#index_heading_back_background').css('background-image', 'url(' + images[nextimage].src + ')');
+    if (mylock != lock) return;
     $('#index_heading_background').fadeOut(1000, function () {
-        $(this).css('background-image', 'url(' + images[currimage].src + ')');
+        $(this).css('background-image', 'url(' + images[nextimage].src + ')');
+        if (mylock != lock) return;
+        nextimage++;
         $(this).toggle(0, function () {
-            setTimeout(nextImage, 5000);
+            timers.push(setTimeout(nextImage, 5000));
         });
     });
 }
 
+function loadSlideshowNav() {
+    for (var i = 0; i < images.length; i++) {
+        $('#index_slideshow_nav_list').append('<li><div id="index_slideshow_nav_image_' + i + '" class="index_slideshow_nav_image unhighlightable">&nbsp;</div></li>');
+    }
+
+    $('#index_slideshow_nav_image_0').addClass('selected');
+
+    $('.index_slideshow_nav_image').click(function () {
+        nextimage = parseInt($(this).attr('id').split('_')[4]);
+        nextImage();
+    })
+}
+
 $(document).ready(function () {
+    loadSlideshowNav();
     setTimeout(nextImage, 5000);
 });
