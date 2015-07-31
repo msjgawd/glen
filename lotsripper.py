bc28d9f146a4ece1e16a5710283490aee32ef7b0
David Hou <david.hou314@gmail.com>
2015-07-31 11:35:30 -0700

    Add ripper tool to get data from theglenatmaplefalls.org's lots for sale page

---
+++ lotsripper.py
@@ -0,0 +1,42 @@
+from jinja2 import Template
+from lxml import html
+import requests
+
+page = requests.get('http://theglenatmaplefalls.com/main-pages/lotsforsale.htm')
+# print page.text
+tree = html.fromstring(page.text)
+lots = []
+i = 0
+for table in tree.xpath('/html/body/div/table | /html/body/div/center/table'):
+    divlots = []
+    f = True
+    for row in table.xpath('tr'):
+        if f:
+            if row.text_content().find('TRAILER') == -1:
+                break
+            f = False
+            i += 1
+            continue
+        lot = {}
+        cells = row.xpath('td')
+        lot['sold'] = cells[6].text_content().strip() == 'SOLD'
+        if lot['sold']:
+            continue
+        lot['new'] = len(cells[0].getchildren()) != 0
+        lot['number'] = int(cells[1].text_content().strip()[3:])
+        lot['trailer'] = cells[3].text_content().strip() == 'yes'
+        lot['shed'] = cells[4].text_content().strip() == 'yes'
+        lot['gazebo'] = cells[5].text_content().strip() == 'yes'
+        pricestring = cells[6].text_content().strip()
+        lot['price'] = -1 if pricestring == 'Pending' else int(
+            pricestring[1:max(pricestring.find('U'), pricestring.find('C'))].replace(',', ''))
+        if lot['price'] >= 0:
+            lot['currency'] = pricestring[max(pricestring.find('U'), pricestring.find('C')):]
+        lot['contact'] = cells[7].text_content().strip().replace('\t', '').replace('\n', '<br/>')
+        divlots.append(lot)
+    print i
+    if not f:
+        lots.append([i, divlots])
+
+lfspage = Template(open('lotstemplate.html', 'r').read())
+open('lots.html', 'w').write(lfspage.render(lots=lots).encode('ascii', 'replace'))
