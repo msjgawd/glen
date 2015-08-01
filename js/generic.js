11e12e51007fe84ac9c3c850c056802c093f9239
David Hou <david.hou314@gmail.com>
2015-07-31 20:03:33 -0700

    Open new tabs with JS instead of target="_blank"

--- js/generic.js
+++ js/generic.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     var logo = '<link rel="shortcut icon" href=images/glenlogo.gif>';
     $('head').prepend(logo);
 
-    var footerhtml = '<div class="footer" id="footer" style="background-color: #334439"><div class="center_vertical height100"><ul id="footer"><li><a href="index.html">Home</a></li><li><a href="maps.html">Maps</a></li><li><a href="contact.html">Contact</a></li><li><a href="facilities.html">Facilities</a></li><li><a href="lots.html">Lots for Sale</a></li><li><a href="#members">Members</a></li><li><a href="pubdocs.html">Public Docs</a></li><li><a href="http://images.drivebc.ca/bchighwaycam/pub/html/www/index-Northern.html" target="_blank">Border Cams</a></li><li><a id="scrolltotop" style="float: right; margin-right: 3em" href="#">&#8593;Back to Top</a></li></ul><hr align="left"><p class="footer">7159 Mount Baker Highway Deming, WA 98244 &nbsp; &vert; &nbsp; Web Design &amp; Development by <a class="footer" target="_blank" href="http://msjwave.com">MSJWAVE 2015</a></p></div></div>';
+    var footerhtml = '<div class="footer" id="footer" style="background-color: #334439"><div class="center_vertical height100"><ul id="footer"><li><a href="index.html">Home</a></li><li><a href="maps.html">Maps</a></li><li><a href="contact.html">Contact</a></li><li><a href="facilities.html">Facilities</a></li><li><a href="lots.html">Lots for Sale</a></li><li><a href="#members">Members</a></li><li><a href="pubdocs.html">Public Docs</a></li><li><a href="http://images.drivebc.ca/bchighwaycam/pub/html/www/index-Northern.html" data-popup="true">Border Cams</a></li><li><a id="scrolltotop" style="float: right; margin-right: 3em" href="#">&#8593;Back to Top</a></li></ul><hr align="left"><p class="footer">7159 Mount Baker Highway Deming, WA 98244 &nbsp; &vert; &nbsp; Web Design &amp; Development by <a class="footer" data-popup="true" href="http://msjwave.com">MSJWAVE 2015</a></p></div></div>';
     $('body').append(footerhtml);
     $('a#scrolltotop').click(function (event) {
         console.log('hei');
@@ -23,4 +23,12 @@ $(document).ready(function () {
 
     adjust_sizes();
     $(window).resize(adjust_sizes);
+});
+
+$(function () {
+    $("a[data-popup]").live('click', function (e) {
+        window.open($(this)[0].href);
+        // Prevent the link from actually being followed
+        e.preventDefault();
+    });
 });
